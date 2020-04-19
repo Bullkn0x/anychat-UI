37c055d56066a86ab2a96f2ba0960c61b37076d4
Qi <qchen125@gmail.com>
2020-04-19 15:17:45 -0400

    modal stuff

--- js/modal.js
+++ js/modal.js
@@ -1,25 +1,64 @@
  // Get the modal
- var modal = document.getElementById("myModal");
+ var $modal = $("#myModal");
                             
  // Get the button that opens the modal
- var btn = document.getElementById("addServer");
+ var $btn = $(".addServer");
  
  // Get the <span> element that closes the modal
- var span = document.getElementsByClassName("close")[0];
+ var span = $("close")[0];
  
- // When the user clicks the button, open the modal 
- btn.onclick = function() {
-   modal.style.display = "block";
+ var $options = $('.search-options');
+
+ //Gets the Create Server button class
+ var createButton = document.querySelector("#createServer");
+
+ //Gets the Modal that creates a new server
+ var createModal= document.getElementById("newServerModal");
+
+ // Select the create button which has 
+ var $createSubmit = $("#createServerSubmit");
+
+ var $serverName = $("#serverName");
+
+ //As Soon as user click submit, exit the modal.
+ $createSubmit.on('click' , function(){
+    if($serverName.val()){
+      createModal.style.display="none";
+      alert($serverName.val())
+      $serverName.clear();
+      var room_name=$serverName.val();
+      socket.emit('create server', {
+        'room_name':room_name
+    });
+    }
+  });
+
+
+
+ createModal.onclick = function(e){
+  if(e.target == e.currentTarget){
+    createModal.style.display="none";
+  }
  }
+
+
+
+ //When the Create a server button is clicked, Modal is displayed.
+ createButton.onclick = function(){
+  createModal.style.display = "block";
+ }
+
+ // When the user clicks the button, open the modal 
+ $btn.on('click', function() {
+   $modal.css('display', "flex");
+   $('#search-heading').addClass('animated fadeIn')
+   $options.addClass('animated slideInDown delay-1s');
+ });
  
  // When the user clicks on <span> (x), close the modal
- span.onclick = function() {
-   modal.style.display = "none";
- }
- 
- // When the user clicks anywhere outside of the modal, close it
- window.onclick = function(event) {
-   if (event.target == modal) {
-     modal.style.display = "none";
-   }
- }
+ $("#myModal").click( function(e) {
+    if(e.target == e.currentTarget) {
+        $modal.css('display', "none");
+    }
+});
+
